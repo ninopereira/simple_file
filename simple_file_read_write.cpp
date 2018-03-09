f6faf840939e1f39a4ab26ded3b4beaa5554c4d5
Nino Pereira <nino.pereira@dyson.com>
2018-03-09 11:31:21 +0000

    feat: add verification step to read and write routines.

--- simple_file_read_write.cpp
+++ simple_file_read_write.cpp
@@ -1,28 +1,54 @@
 #include <fstream>
 #include <iostream>
 
-void write_file()
+int write_file()
 {
-    std::ofstream myfile;
+    bool error = 0;
+	std::ofstream myfile;
     myfile.open ("my_file.txt");
-    myfile << "1,2,3\n" ;
-    myfile.close();
+	if (myfile.is_open())
+	{
+		myfile << "1,2,3\n" ;
+		myfile.close();
+	}
+	else
+	{
+		std::cout << "Error opening file" << std::endl;
+		error = 1;
+	}
+    return error;
 }
 
-void read_file()
+int read_file()
 {
-    std::ifstream myfile;
+    int error = 0;
+	std::ifstream myfile;
     myfile.open ("my_file.txt");
-    double a,b,c;
-    char comma;
-    myfile >> a >> comma >> b >> comma >> c;
-    std::cout << " I got this: " <<  a << " --- " << b << " --- " << c << std::endl;
-    myfile.close();
+	if (myfile.is_open())
+	{
+		double a,b,c;
+		char comma;
+		myfile >> a >> comma >> b >> comma >> c;
+		std::cout << " I got this: " <<  a << " --- " << b << " --- " << c << std::endl;
+		myfile.close();
+	}
+	else
+	{
+
+		std::cout << "Error opening file" << std::endl;
+		error = 1;
+	}
+	return error;
 }
 
 int main()
 {
-	write_file();
-	read_file();
+	int error = 0;	
+	error = write_file();
+	if (!error)
+	{	
+		read_file();
+	}
+	return error;
 }
 
